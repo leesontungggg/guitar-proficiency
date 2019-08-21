83ca078f21c929e537a10075233dc3375796af93
huylhDSV <huylh@dgroup.co>
2019-08-21 17:17:44 +0700

    -Change color for status bar

--- android/app/src/main/java/com/guitar_proficiency/MainActivity.java
+++ android/app/src/main/java/com/guitar_proficiency/MainActivity.java
@@ -1,19 +1,25 @@
 package com.guitar_proficiency;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
 
-public class MainActivity extends ReactActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this,R.style.SplashScreenTheme);  // here
-        super.onCreate(savedInstanceState);
-    }
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
+    public class MainActivity extends ReactActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            SplashScreen.show(this,R.style.SplashScreenTheme);  // here
+            super.onCreate(savedInstanceState);
+        }
+        /**
+         * Returns the name of the main component registered from JavaScript.
+         * This is used to schedule rendering of the component.
+         */
     @Override
     protected String getMainComponentName() {
         return "GUITAR_PROFICIENCY";
